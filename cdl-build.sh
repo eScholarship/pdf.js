a5c58f685c42a1386628ee9ca44df4ef6013f447
Martin Haye <martin.haye@ucop.edu>
2019-08-09 17:28:03 -0700

    Rebuild 2.2.228 worker and locale data.

--- cdl-build.sh
+++ cdl-build.sh
@@ -18,6 +18,7 @@ cp build/version.json dist/
 VERSION=`grep 'version' build/version.json | sed 's/[^0-9.]//g'`
 cp build/minified/build/pdf.worker.js dist/pdf.worker-$VERSION-min.js
 cp build/minified/build/pdf.worker.js.map dist/pdf.worker-$VERSION-min.js.map
+cp -r build/minified/web/locale dist/locale
 sed 's/images\//\/node_modules\/pdfjs-embed2\/web\/images\//g' build/minified/web/viewer.css > dist/viewer.css
 
 echo "Done. Version: $VERSION"
